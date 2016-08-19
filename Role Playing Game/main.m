246ae8b7224395aa5f3d3c19e59d0d077c532b31
Dan Esrey <danesrey@dans-macbook-air.local>
2016-08-19 10:34:46 -0400

    added clothing choice to main

--- Role Playing Game/main.m
+++ Role Playing Game/main.m
@@ -9,19 +9,44 @@
 #import <Foundation/Foundation.h>
 #import "Userinput.h"
 #import "Character.h"
+#import "some enum.h"
 
 int getUserName(char userName[], char *prompt);
 
 int main(int argc, const char * argv[]) {
-   
+    
     char userName[100] = "\0";
     int numberOfItemsScanned = 0;
     while (numberOfItemsScanned != 1) {
-        numberOfItemsScanned = getUserName(userName, "What is your name?\n");
-    }    
-    @autoreleasepool {
-        NSLog(@"Hello %s", userName);
+        numberOfItemsScanned = getUserName(userName, "What is your character name?\n");
     }
+    @autoreleasepool {
+        NSLog(@"Hello %s\n", userName);
+    }
+    
+    printf("What would you like your character to wear?\n");
+    
+    Outerwear outerwearChosen = OuterwearNull;
+    int numberOfClothingItemsScanned = 0;
+    
+    while ((numberOfClothingItemsScanned != 1) ||
+           (outerwearChosen < OuterwearFirst) || (outerwearChosen > OuterwearLast)) {
+        fpurge(stdin);
+        printf("\nPlease enter:\n");
+        printf("    %d for Cloak\n", OuterwearCloak);
+        printf("    %d for Hooded Robe\n", OuterwearHooded_Robe);
+        printf("    %d for Body Armor\n", OuterwearBody_Armor);
+        printf("    %d for Trenchcoat\n", OuterwearTrenchcoat);
+        printf("    %d for EVA Suit\n", OuterwearEVA_Suit);
+        printf("    %d for Leather Jacket\n", OuterwearLeather_Jacket);
+        
+        numberOfClothingItemsScanned = scanf("%d", &outerwearChosen);
+    }
+    
+    char *outerwearAsString = OuterwearGetStringName(outerwearChosen);
+    printf("\nYou chose the outerwear %s\n\n", outerwearAsString);
+    
+    
     return 0;
     
 }
